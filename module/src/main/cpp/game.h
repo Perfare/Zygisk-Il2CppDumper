0091b98645b9199aa2690ed645c44f29cd30d053
budu1985328271 <100823030+budu1985328271@users.noreply.github.com>
2023-10-23 17:01:24 +0800

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "com.yxyz.game"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
